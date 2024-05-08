85ebbc6ac940fc1addd6bc7c3dfe3221e79db480
Serbanescu Bogdan <sherbibv@gmail.com>
2024-05-08 23:23:20 +0000

    chore: add portainer update script

---
+++ portainer/update-portainer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker stop portainer
+docker rm portainer
+docker pull portainer/portainer-ce:latest
+
+docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
