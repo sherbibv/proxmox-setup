02257a3f5fa0fd472928c151d98585a4322ac62d
Serbanescu Bogdan <sherbibv@gmail.com>
2024-05-08 23:26:04 +0000

    chore: update script

--- portainer/update-portainer.sh
+++ portainer/update-portainer.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+echo "Instalation started!"
 docker stop portainer
 docker rm portainer
+
+echo "Pulling portainer"
+
 docker pull portainer/portainer-ce:latest
 
+echo "Starting portainer"
 docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+echo "Instalation finished!"
