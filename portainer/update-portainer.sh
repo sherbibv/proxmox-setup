4b208c86af25c195b542a006a6d66d506e0370ac
Serbanescu Bogdan <sherbibv@gmail.com>
2024-05-30 12:24:54 +0300

    Update update-portainer.sh

--- portainer/update-portainer.sh
+++ portainer/update-portainer.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Instalation started!"
+# Use "latest" as the default version tag if none is provided
+VERSION_TAG=${1:-latest}
+
+echo "Installation started!"
 docker stop portainer
 docker rm portainer
 
 echo "Pulling portainer"
 
-docker pull portainer/portainer-ce:latest
+docker pull portainer/portainer-ce:$VERSION_TAG
 
 echo "Starting portainer"
-docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:$VERSION_TAG
 
-echo "Instalation finished!"
+echo "Installation finished with version: $VERSION_TAG"
